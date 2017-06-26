2fcfd8b6fbf0d02571a8e67c5387606958c1bf2e
dylanstudent2 <dtsod765@gmail.com>
2017-06-26 16:20:06 -0500

    Refreshing master-at-rebate

--- geode-core/src/main/java/org/apache/geode/cache/query/internal/CompiledLoad.java
+++ geode-core/src/main/java/org/apache/geode/cache/query/internal/CompiledLoad.java
@@ -49,7 +49,14 @@ import org.apache.geode.internal.i18n.LocalizedStrings;
 import org.apache.geode.internal.logging.LogService;
 import org.apache.geode.pdx.PdxInstance;
 import org.apache.geode.pdx.internal.PdxString;
+import org.apache.geode.vector.Vector;
 import org.apache.logging.log4j.Logger;
+import java.util.Map;
+import entagged.audioformats.*; //Use a jar program!
+import java.util.LinkedList;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  * Created by dylan on 6/25/17.
@@ -58,10 +65,12 @@ public class CompiledLoad extends AbstractCompiledValue{
     private CompiledID filePath;
     private CompiledRegion region;
     final Logger logger = LogService.getLogger();
+    private Vector item = null;
 
     public CompiledLoad(CompiledID filePath, CompiledRegion region){
         this.filePath = filePath;
         this.region = region;
+
 
     }
         //Set up a collection interface called LoadResults later
@@ -69,14 +78,46 @@ public class CompiledLoad extends AbstractCompiledValue{
             NameResolutionException, QueryInvocationTargetException {
         Object duh = null;
         logger.info("----WITHIN THE EVALUATE OF LOAD!!!!----");
+
+        // Vector stream = new Vector(20);
+        String [] mp3Info = new String[8];
+        LinkedList <String []> list = new LinkedList<String []> ();
+        //Attach a '/' if dirName is missing one
+        String ele = new String();
+
+        try{
+            Scanner csv = new Scanner(new File(this.filePath.getId() + "mp3.csv"));
+            while(csv.hasNext()){
+                ele = csv.nextLine();
+                logger.info("WE HAVE: " + ele);
+                //System.out.println(ele + " ------ spot: " + vEnd + " -----"  + ele.substring( 0 ,ele.indexOf(vEnd)));
+                //list.add(ele.split(","));
+                //list.add(ele.substring(0, ele.indexOf(vEnd)).split(","));
+                //WIth the array of stuff stored, go through each index and replace all delim's with a comma.
+                //Instead of a direct add, have the ele.substring..... be stored within an array.
+                //Then Set up a vector type with ele.substring(ele.indexOf(vEnd) and have both components
+                // stored within a mp3Obj then add that to the list!
+            }
+        }
+        catch(Exception e){
+            System.err.println("Error1: " + e.getMessage());
+            e.printStackTrace();
+        }//catch
+
+
+
+
+
+
         return duh;
     }
 
+    //Simple getType method. Used by other things no doubt to check if it's a load query.
     public int getType() {
         return LITERAL_load;
     }
 
-
+    //Used by DataCommandFunction.java. Need to have a regionPath added to the set that it's checking for!
     public void getRegionsInQuery(Set regionNames, Object[] parameters){
         regionNames.add(this.region.getRegionPath());
     }
