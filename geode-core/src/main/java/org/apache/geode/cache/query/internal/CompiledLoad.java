9d322b72785d59f94aba4e16f9f95ad267f7db20
dylanstudent2 <dtsod765@gmail.com>
2017-06-27 16:30:35 -0500

    Refreshing master-at-rebate

--- geode-core/src/main/java/org/apache/geode/cache/query/internal/CompiledLoad.java
+++ geode-core/src/main/java/org/apache/geode/cache/query/internal/CompiledLoad.java
@@ -84,10 +84,10 @@ public class CompiledLoad extends AbstractCompiledValue{
         LinkedList <String []> list = new LinkedList<String []> ();
         //Attach a '/' if dirName is missing one
         String ele = new String() ;
-        logger.info("----i We get : " + this.filePath.toString() + "mp3.csv" + System.getProperty("user.dir") + "/../" + this.filePath.toString() + "mp3.csv");
+        logger.info("----i We get : " + System.getProperty("user.dir") + "/../" + this.filePath.getSavedPdxString().toString());
         try{
             //Obtain the csv file from wherever directory it's located in.
-            Scanner csv = new Scanner(new File(System.getProperty("user.dir") + "/../" + this.filePath.getSavedPdxString().toString() + "/mp3.csv"));
+            Scanner csv = new Scanner(new File(System.getProperty("user.dir") + "/../" + this.filePath.getSavedPdxString().toString()));
             while(csv.hasNext()){
                 ele = csv.nextLine();
                 logger.info("WE HAVE: " + ele);
